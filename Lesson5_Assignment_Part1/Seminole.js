93ca27a698207f6d1633c1afcd5275baabbe6da6
GMuller <gam13k@my.fsu.edu>
2015-10-07 17:06:54 +0000

    Lesson 5 Assignment - Final Commit

--- Lesson5_Assignment_Part1/Seminole.js
+++ Lesson5_Assignment_Part1/Seminole.js
@@ -24,16 +24,12 @@ var opponents = ["Lightning", "Combines", "Combines",
     "(off)", "(off)"];
 
 // global variables
-var opponents = ["Lightning", "Combines", "Combines", 
-    "Combines", "Lightning", "Lightning", "Lightning", 
-    "Lightning", "Barn Raisers", "Barn Raisers", 
-    "Barn Raisers", "Sodbusters", "Sodbusters", "Sodbusters",
-    "Sodbusters", "(off)", "River Riders", "River Riders", 
-    "River Riders", "Big Dippers", "Big Dippers", 
-    "Big Dippers", "(off)", "Sodbusters", "Sodbusters",
-    "Sodbusters", "Combines", "Combines", "Combines", 
-    "(off)", "(off)"];
-
+var gameLocation = 
+   ["away", "away", "away", "away", "home", "home", "home",
+    "home", "home", "home", "home", "away", "away", "away",
+    "away", "", "away", "away", "away", "away", "away",
+    "away", "", "home", "home", "home", "home", "home",
+    "home", "", ""];
 
 // function to place daysOfWeek values in header row cells 
 function addColumnHeaders() {
@@ -45,12 +41,11 @@ function addColumnHeaders() {
    }//end of while loop
 }//end of addColumnHeaders function
 
-//This statement will add the column headers to the page immediately when the page loads in the browser
 
-// function to place day of month value in first p element 
 
 // within each table data cell that has an id 
 function addCalendarDates() {
+
    var i = 1;
    var paragraphs = "";
    do {
@@ -60,12 +55,35 @@ function addCalendarDates() {
       i++;      
    } while (i < 32);
 }//end of addCalendarDates function 
-
-window.addEventListener("load", setUpPage(), false);
+// function to place opponents and gameLocation values in 
+// second p element within each table data cell that has an id
+function addGameInfo() {
+   var paragraphs = "";
+   for (var i = 0; i < 31; i++) {
+      var date = i+1;
+      var tableCell = document.getElementById("08-" + date);
+      paragraphs = tableCell.getElementsByTagName("p");
+      paragraphs[1].innerHTML += opponents[i];
+     if (gameLocation[i] === "away") { 
+       paragraphs[1].innerHTML = "@ ";
+    paragraphs[1].innerHTML += opponents[i];
+       
+      
+    }
+     else if (gameLocation[i] === "home") {
+             paragraphs[1].innerHTML = "vs ";
+             paragraphs[1].innerHTML += opponents[i];
+ }//end of else if
+     //end of if 
+   }//end of the for loop
+}//end of the function
 
 // function to load the calendar content in the calendar when the page loads
 function setUpPage() {
    addColumnHeaders();
    addCalendarDates();
+  addGameInfo();
 }//end of setUpPage function
 
+//window.addEventListener("load", addColumnHeaders(), false);
+window.addEventListener("load", setUpPage(), false);
